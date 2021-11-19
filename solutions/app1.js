dbbefbd4d4ec6bf967cd8d84869037cd15632576
TKende <kendetompos@gmail.com>
2021-11-19 18:19:18 +0100

    done

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,13 @@
+function converterPromise(strings) {
+    const returnPromis = new Promise((resolve, reject) => {
+        if (strings.every(i => (typeof i === "string"))) {
+            resolve(strings.map(string => string.toUpperCase()))
+        } else {
+            reject("Error: Not all elements are string type!")
+        }
+    })
+    return returnPromis;
+}
 
 
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,16 @@
+function sorterPromise(strings) {
+    const returnPromis = new Promise((resolve, reject) => {
+        if (strings.every(i => (typeof i === "string"))) {
+            resolve(strings.sort())
+        } else {
+            reject("Error: Not all elements are string type!")
+        }
+    })
+    return returnPromis;
+}
 
 
 
 
 
-
-// export default sorterPromise;
+export default sorterPromise;
